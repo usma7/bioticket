7bc27e4397509f31ed8a1a79aabbf7fa3e41db93
Juanjoc12 <coche-9@hotmail.com>
2019-10-29 09:31:05 -0500

    Se agrega Registro de usuario

--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import CrearEvento from './CrearEvento';
 import VisualizarEventos from './VisualizarEvento';
+import RegistrarUsuario from './RegistrarUsuario';
 class Sidebar extends Component {
     constructor() {
         super();
         this.state = {
-            estshowCrear: true,
-            estshowVisualizar: false
+            estshowCrear: false,
+            estshowVisualizar: false,
+            estshowRegistrar: true
         };
     }
     showCrear() {
-        this.setState({ estshowCrear: true, estshowVisualizar: false});
+        this.setState({ estshowCrear: true, estshowVisualizar: false, estshowRegistrar: false});
     }
     showVisualizar() {
-        this.setState({ estshowCrear: false, estshowVisualizar: true});
+        this.setState({ estshowCrear: false, estshowVisualizar: true, estshowRegistrar: false});
+    }
+    showRegistrar() {
+        this.setState({ estshowCrear: false, estshowVisualizar: false, estshowRegistrar: true});
     }
     render() {
         return (
@@ -37,7 +42,7 @@ class Sidebar extends Component {
                             <a type="button" onClick={this.showVisualizar.bind(this)}>Consultar Eventos</a>
                         </li>
                         <li>
-                            <a type="button" onClick="#">Noticias</a>
+                            <a type="button" onClick={this.showRegistrar.bind(this)}>Registrar Usuario</a>
                         </li>
                     </ul>
                 </nav>
@@ -48,6 +53,7 @@ class Sidebar extends Component {
                         <div className="container-fluid">
                             {this.state.estshowVisualizar && <VisualizarEventos />}
                             {this.state.estshowCrear && <CrearEvento />}
+                            {this.state.estshowRegistrar && <RegistrarUsuario />}
                         </div>
                     </nav>
 
